3190872e2dfe71da590675bbc61b7bf4bd3a9360
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-03-29 00:42:47 +0530

    Added function is_odd and used is even in it

--- src/basic_function.c
+++ src/basic_function.c
@@ -4,9 +4,16 @@ unsigned char is_even(int number) {
   return number % 2 == 0;
 }
 
+unsigned char is_odd(int number) {
+  return ! is_even(number);
+}
+
 int main(void) {
   int number;
-  printf("enter a number\n");
+  printf("enter a number to check if it is even\n");
   scanf("%d", &number);
-  printf("%d is %s \n", number, is_even(number) ? "Even" : "Not even");
+  printf("%d is %s \n", number, is_even(number) ? "even" : "Not even");
+  printf("enter a number to check if it is odd\n");
+  scanf("%d", &number);
+  printf("%d is %s \n", number, is_odd(number) ? "odd" : "Not odd");
 }
