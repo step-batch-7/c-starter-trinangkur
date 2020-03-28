da54cca0fe192bb192eaa0ee526d659160b26fc8
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-03-29 00:49:47 +0530

    Added function to find square of a given number

--- src/basic_function.c
+++ src/basic_function.c
@@ -8,6 +8,10 @@ unsigned char is_odd(int number) {
   return ! is_even(number);
 }
 
+int square(int number) {
+  return number * number;
+}
+
 int main(void) {
   int number;
   printf("enter a number to check if it is even\n");
@@ -16,4 +20,7 @@ int main(void) {
   printf("enter a number to check if it is odd\n");
   scanf("%d", &number);
   printf("%d is %s \n", number, is_odd(number) ? "odd" : "Not odd");
+  printf("enter a number to find square of it");
+  scanf("%d", &number);
+  printf("square of %d is %d\n", number, square(number));
 }
