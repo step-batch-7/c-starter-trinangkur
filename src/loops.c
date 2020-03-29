13a200b14dd9e66ca17fa3f9e293772cfd5de7f2
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-03-29 16:00:51 +0530

    Added function for printing fibonacci series for given n

--- src/loops.c
+++ src/loops.c
@@ -8,10 +8,25 @@ unsigned long int factorial(int number) {
   return fact;
 }
 
+void print_fibonacci_series(int terms) {
+  int num1 = -1, num2 = 1, term, sum;
+  for (term = 1; term <= terms; term++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+    printf("%d ", num2);
+  }
+  printf("\n");
+}
 
 int main (void) {
   int number;
+
   printf("Enter a number to find factorial of it\n");
   scanf("%d", &number);
   printf("the factorial is %lu\n", factorial(number));
+
+  printf("find fibonacci series of n terms, enter value of n\n");
+  scanf("%d", &number);
+  print_fibonacci_series(number);
 }
